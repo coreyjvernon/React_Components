06bb4f785e60b12f0842144fd202de7654a8e357
Corey Vernon <coreyjvernon@gmail.com>
2019-03-03 12:52:39 -0500

    fixed react components assignment

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import projects from '../data/projects.json';
-
+import Project from './Project';
 
 class Projects extends Component {
+    render() {
+        
 
+        
+        return(
+            <div>
+                {projects.allProjects.map((item) => {
+                return <Project key={item.title} title={item.title} imgName={item.imgName} description={item.description} />
+                } )}
+            </div>
+        )
+    }
 }
 
 export default Projects;
--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 
-
 class SocialLinks extends Component {
+    render() {
 
+        let classes = this.props.imgName + " porfolio-img" ;
+
+        return(
+            <div>
+                <h2>{ this.props.title }</h2>
+                <div className={ classes } ></div>
+                <p>{ this.props.description }</p>
+            </div>
+        )
+    }
 }
 
 export default SocialLinks;
